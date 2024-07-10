250ebbe009a0155e799077bc77c48718967c2c44
akowa <akowa344@gmail.com>
2024-07-09 21:49:39 -0400

    maven commit

--- src/test/java/tek/chrome/Activity2.java
+++ src/test/java/tek/chrome/Activity2.java
@@ -2,13 +2,13 @@ package tek.chrome;
 
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class FirstSeleniumClass {
+public class Activity2 {
     public static void main(String[] args) {
         ChromeDriver chromeDriver = new ChromeDriver();
         chromeDriver.manage().window().maximize();
-        chromeDriver.get("https://facebook.com");
+        chromeDriver.get("https://dev.insurance.tekschool-students.com/");
         String title = chromeDriver.getTitle();
         System.out.println(title);
-       // chromeDriver.quit();
+        chromeDriver.quit();
     }
 }
